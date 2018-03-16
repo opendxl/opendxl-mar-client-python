c04f15831c6c59dd34a6c393c90e97fd69d7f479
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-03-16 10:20:28 -0700

    Update pylint glob to parse all sample files

--- setup.py
+++ setup.py
@@ -32,6 +32,10 @@ class LintCommand(Command):
         self.announce("Running pylint for library source files and tests",
                       level=distutils.log.INFO)
         subprocess.check_call(["pylint", "dxlmarclient"] + glob.glob("*.py"))
+        self.announce("Running pylint for samples", level=distutils.log.INFO)
+        subprocess.check_call(["pylint"] + glob.glob("sample/*.py") +
+                              glob.glob("sample/**/*.py") +
+                              ["--rcfile", ".pylintrc.samples"])
 
 
 class CiCommand(Command):
@@ -73,7 +77,7 @@ setup(
     },
 
     # Application author details:
-    author="McAfee, Inc.",
+    author="McAfee LLC",
 
     # License
     license="Apache License 2.0",
